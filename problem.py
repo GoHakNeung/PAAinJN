ca117a96808d54114f9e85e575151f7ac74bb3d9
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2023-02-25 05:25:50 +0900

    Create problem.py

---
+++ problem.py
@@ -0,0 +1,28 @@
+question_1 ='''<h2 style = "background-color:yellow; ">Description</h2>
+<p>Let's create a program to print out whether <span style = "background-color : #E0E0E0">one number</span> you inputted is obtuse, right, or acute.</p>
+<HR>
+<h2> input
+</h2>
+<p>A integer between 1 and 179 is inputted</p>
+<p>Units(&#176;) are omitted.</p>
+<h2> output
+</h2>
+<p>Outputs one of acute, right, or obtuse.</p>
+<HR>
+<div style = "float:left;width:50%">
+<h2> example input </h2>
+<p>56</p>
+</div>
+<div style = "float:right;width:50%">
+<h2> output example </h2>
+<p>acute</p>
+</div>
+'''
+img_1 = "https://github.com/GoHakNeung/python/blob/main/img_4201.jpg?raw=true"
+answer_1 = [
+    {'input' : [[1]], 'output' : ['acute']},
+    {'input' : [[89]], 'output' : ['acute']},
+    {'input' : [[90]], 'output' : ['right']},
+    {'input' : [[91]], 'output' : ['obtuse']},
+    {'input' : [[179]], 'output' : ['obtuse']}
+]
