2944c464e372e073d529ad11f553b474f7e96fcc
GoHakNeung <98439777+GoHakNeung@users.noreply.github.com>
2024-05-08 04:03:05 +0900

    Update code_check.py

--- code_check.py
+++ code_check.py
@@ -1206,11 +1206,11 @@ def table_check(py) :
     if df.shape == df_answer.shape : 
       global table_count
       table_count = 0
-      df_html_color = df.style.applymap(table_compare, df_answer = df_answer).to_html().replace('<table', '<table class = "dataframe"')
       pd.set_option('display.max_rows', 10)
       pd.set_option('display.max_columns', 10)
       pd.options.display.float_format = "{:.2f}".format
-
+      
+      df_html_color = df.style.applymap(table_compare, df_answer = df_answer).to_html().replace('<table', '<table class = "dataframe"')
       output_html_color = f'''
       <div style="display: flex; flex-direction: row;">
           <div style="float:left;width:50%">
